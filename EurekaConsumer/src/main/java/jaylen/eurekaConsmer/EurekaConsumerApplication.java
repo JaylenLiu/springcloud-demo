9bba6bb0efb21219cd8bfa249fe4356b2e51e714
ljl <1003800834@qq.com>
2019-05-08 14:19:24 +0800

    在模块EurekaConsumer中集成了Ribbon和Hystrix

--- EurekaConsumer/src/main/java/jaylen/eurekaConsmer/EurekaConsumerApplication.java
+++ EurekaConsumer/src/main/java/jaylen/eurekaConsmer/EurekaConsumerApplication.java
@@ -1,8 +1,8 @@
 package jaylen.eurekaConsmer;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableCircuitBreaker
 @EnableDiscoveryClient
 public class EurekaConsumerApplication {
 
@@ -19,6 +19,8 @@ public class EurekaConsumerApplication {
     RestTemplate restTemplate(){
         return new RestTemplate();
     }
+
+
     public static void main(String[] args) {
         SpringApplication.run(EurekaConsumerApplication.class, args);
     }
--- EurekaConsumer/src/main/java/jaylen/eurekaConsmer/controller/ConsumerController.java
+++ EurekaConsumer/src/main/java/jaylen/eurekaConsmer/controller/ConsumerController.java
@@ -1,5 +1,6 @@
 package jaylen.eurekaConsmer.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +13,12 @@ public class ConsumerController {
     RestTemplate restTemplate;
 
     @GetMapping("/consumer-hello")
+    @HystrixCommand(fallbackMethod = "error")
     public String consumerTest(){
         return restTemplate.getForEntity("http://eureka-client/index", String.class).getBody();
+    }
+
+    public String error(){
+        return "error";
     }
 }
