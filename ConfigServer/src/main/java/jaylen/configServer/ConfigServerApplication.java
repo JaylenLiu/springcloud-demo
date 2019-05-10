a5223fa0499bf6aab876c8cd851dd9241a983dec
ljl <1003800834@qq.com>
2019-05-10 15:06:39 +0800

    spring cloud config server

---
+++ ConfigServer/src/main/java/jaylen/configServer/ConfigServerApplication.java
@@ -0,0 +1,18 @@
+package jaylen.configServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @Author: Jaylen
+ * @Description:
+ * @Date: 2019/5/10 10:41
+ */
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
